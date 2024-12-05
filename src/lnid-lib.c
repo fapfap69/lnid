9c6840a8dec4aa082e106561ff53b0e7e071a1ea
Antonio Franco <antonio.franco@cern.ch>
2024-12-05 12:38:16 +0100

    Fix

---
+++ src/lnid-lib.c
@@ -0,0 +1,349 @@
+/* ----------------------------------------------------------
+    LNID - Local Network Identity Discovery
+
+   Libreria funzioni
+
+ Copyright (c) 2024 Antonio Franco
+
+ Questo programma è rilasciato sotto la licenza Creative Commons Attribuzione 4.0 Internazionale (CC BY 4.0).
+ Puoi condividere, copiare, distribuire e modificare il programma, a condizione di dare il dovuto credito all'autore originale.
+
+ Licenza completa: https://creativecommons.org/licenses/by/4.0/
+ 
+ auth. A.Franco - INFN Bary Italy
+ date: 28/11/2024       ver.1.1
+
+ ---------------------------------------------------------
+  HISTORY 
+  28/11/2024  -  Creation
+
+ ---------------------------------------------------------
+*/
+#ifndef __GENERICLIB__
+#define __GENERICLIB__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/ioctl.h>
+#include <netdb.h>
+#include <errno.h>
+#include <sys/select.h>
+#include <net/if.h>
+
+#if defined(__APPLE__)
+#include <ifaddrs.h>
+#include <netinet/in.h>
+#include <net/ethernet.h>  // Necessario per LLADDR
+#include <net/if_dl.h>
+#endif
+
+
+int isVerbose = 0;
+
+// --- The SSL support
+#include "lnid-ssl.c"
+
+// --- Web server ---
+#define DEFAULT_PORT 16969 // Porta su cui ascoltare
+#define BUFFER_SIZE 4096
+#define MAX_CLIENTS 100
+#define TIMEOUT_SEC 2  // Timeout in secondi
+#define TIMEOUT_USEC 0 // Timeout in microsecondi
+#define TRUE 1
+#define FALSE 0
+
+// Funzione per ottenere l'hostname
+char* get_hostname() {
+    static char hostname[256];
+    if (gethostname(hostname, sizeof(hostname)) == 0) {
+        return hostname;
+    } else {
+        perror("gethostname");
+        return "Unknown";
+    }
+}
+
+// Funzione per ottenere un ID univoco (può essere il PID o qualsiasi altra cosa)
+char* get_unique_id() {
+    static char id[256];
+    snprintf(id, sizeof(id), "ID-%d", getpid()); // Utilizza il PID come ID
+    return id;
+}
+
+// Funzione per ottenere il MAC address da usare come ID
+char *get_macaddr_id(char *theEthernetMAC) {
+    static char id[256];
+    unsigned char *mac = NULL;
+
+#ifdef __linux__
+    int sock;
+    struct ifreq ifr;
+
+    // Crea un socket di tipo AF_INET
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock == -1) {
+        perror("Errore durante la creazione del socket");
+        exit(EXIT_FAILURE);
+    }
+
+    // Copia il nome dell'interfaccia nella struttura ifreq
+    strncpy(ifr.ifr_name, theEthernetMAC, IFNAMSIZ - 1);
+
+    // Usa ioctl per ottenere il MAC address
+    if (ioctl(sock, SIOCGIFHWADDR, &ifr) == -1) {
+        perror("Errore durante l'ottenimento del MAC address");
+        close(sock);
+        exit(EXIT_FAILURE);
+    }
+
+    // Estrae il MAC address dalla struttura ifreq
+    mac = (unsigned char *)ifr.ifr_hwaddr.sa_data;
+
+    // Stampa il MAC address in formato esadecimale
+    if(isVerbose) fprintf(stdout,"MAC address di %s: %02x:%02x:%02x:%02x:%02x:%02x\n",
+           ifr.ifr_name,
+           mac[0], mac[1], mac[2],
+           mac[3], mac[4], mac[5]);
+
+    close(sock);
+
+#elif defined(__APPLE__)
+    struct ifaddrs *ifaddr, *ifa;
+    
+    // Ottieni tutte le informazioni sulle interfacce di rete
+    if (getifaddrs(&ifaddr) == -1) {
+        perror("getifaddrs");
+        exit(EXIT_FAILURE);
+    }
+
+    // Scorri le interfacce
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+        // Controlla se l'interfaccia corrisponde a quella richiesta
+        if (ifa->ifa_addr == NULL) {
+            continue;
+        }
+
+        // Verifica se l'interfaccia è di tipo AF_LINK (incluso il MAC address)
+        if (ifa->ifa_addr->sa_family == AF_LINK && strcmp(ifa->ifa_name, theEthernetMAC) == 0) {
+            struct sockaddr_dl *sdl = (struct sockaddr_dl *) ifa->ifa_addr;
+            mac = (unsigned char *)sdl->sdl_data + 6;  // I primi 6 byte sono l'indirizzo MAC
+
+            // Stampa l'indirizzo MAC in formato esadecimale
+            fprintf(stdout,"MAC address di %s: %02x:%02x:%02x:%02x:%02x:%02x\n",
+                   ifa->ifa_name,
+                   mac[0], mac[1], mac[2],
+                   mac[3], mac[4], mac[5]);
+            break;
+        }
+    }
+    freeifaddrs(ifaddr);
+#else
+    fprintf(stderr, "Questo programma non è supportato su questo sistema operativo.\n");
+    exit(EXIT_FAILURE);
+#endif
+    if(mac == NULL) {
+        snprintf(id, sizeof(id), "00:00:00:00:00:00");    
+    } else {
+        snprintf(id, sizeof(id), "%02x:%02x:%02x:%02x:%02x:%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);    
+    }
+    return id;
+}
+
+// ---- Legge un intero file in un buffer di memoria
+//
+size_t readAllFile(char *fileName, char *buffer) {
+    size_t fsize = 0;
+    FILE *fp = fopen(fileName, "rb");
+    if(fp == NULL) return(fsize);
+    fseek(fp, 0, SEEK_END);
+    fsize = ftell(fp);
+    fseek(fp, 0, SEEK_SET); 
+    if(fsize > BUFFER_SIZE) {
+        fprintf(stdout,"readAllFile() : file eccedente la massima dimensione dei buffer !"); 
+        fsize = 0;
+    } else {
+        size_t re = fread(buffer, 1, fsize, fp);
+        if(re != fsize) {
+            fprintf(stdout,"readAllFile() : errore in lettura (%lu->%lu)!\n",fsize,re); 
+            fsize = 0;
+        } 
+    }
+    fclose(fp);
+    return(fsize);
+}
+
+int writeAllFile(char *fileName, char *buffer, size_t len) {
+    FILE *fp = fopen(fileName, "w");
+    if(fp == NULL) return(FALSE);
+    size_t wr = fwrite(buffer, 1, len, fp);
+    fclose(fp);
+    if(wr != len) return(FALSE);
+    else return(TRUE);
+}
+
+void creaIlSocket(int *sockfd, struct timeval *timeout, struct sockaddr_in *server_addr, 
+                    int theListeningPort, char *theServerIp ) {
+
+    // Creazione del socket UDP
+    if ((*sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    if(isVerbose) fprintf(stdout,"Socket creato ...\n");
+
+    // Imposta il timeout per il socket
+    timeout->tv_sec = TIMEOUT_SEC;
+    timeout->tv_usec = TIMEOUT_USEC;
+    if (setsockopt(*sockfd, SOL_SOCKET, SO_RCVTIMEO, timeout, sizeof(*timeout)) < 0) {
+        perror("Errore nell'impostazione del timeout");
+        close(*sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    // Inizializzazione dell'indirizzo del server
+    memset(server_addr, 0, sizeof(*server_addr));
+    server_addr->sin_family = AF_INET;
+    server_addr->sin_port = htons(theListeningPort);
+    server_addr->sin_addr.s_addr = inet_addr(theServerIp);
+    return;
+}
+
+void creaIlServerSocket(int *sockfd, struct sockaddr_in *server_addr, 
+                        fd_set *read_fds, int theListeningPort ) {
+
+    // Creazione del socket UDP
+    if ((*sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    if(isVerbose) fprintf(stdout,"Socket creato \n");
+
+    // Inizializzazione della struttura dell'indirizzo del server
+    memset(server_addr, 0, sizeof(*server_addr));
+    server_addr->sin_family = AF_INET;
+    server_addr->sin_addr.s_addr = INADDR_ANY; // Accetta connessioni su tutte le interfacce
+    server_addr->sin_port = htons(theListeningPort);
+
+    // Binding del socket all'indirizzo e alla porta
+    if (bind(*sockfd, (const struct sockaddr*)server_addr, sizeof(*server_addr)) < 0) {
+        perror("bind");
+        close(*sockfd);
+        exit(EXIT_FAILURE);
+    }
+
+    FD_ZERO(read_fds);
+    FD_SET(*sockfd, read_fds);
+    return;
+}
+
+// ---- Ricezione della risposta con eventuale decriptatura
+//
+int rxData(int sockfd, char *buffer, size_t *recv_len, 
+            char *ip_address, EVP_PKEY *keypair) {
+
+    *recv_len = recvfrom(sockfd, buffer, BUFFER_SIZE, 0, NULL, NULL);
+    if (*recv_len < 0) {
+        if (errno == EWOULDBLOCK || errno == EAGAIN) {
+            if(isVerbose) fprintf(stdout,"Timeout raggiunto per %s\n", ip_address);
+        } else {
+            if(isVerbose) fprintf(stdout,"Errore durante la ricezione per %s", ip_address);
+        }
+        // Nessuna risposta ricevuta
+        close(sockfd);
+        return(FALSE);
+    }
+    buffer[*recv_len] = '\0'; 
+    if(keypair != NULL) { // i dati sono criptati 
+        unsigned char *decr;
+        size_t lm;  
+        if(doDecrypt(keypair, (const unsigned char *)buffer, *recv_len,
+                     &decr, &lm) == FALSE) {
+            fprintf(stderr,"rxData() : Errore di decifratura!");
+            return(FALSE);
+        }      
+        if(decr != NULL) memcpy(buffer, decr, lm); 
+        buffer[lm] = '\0';
+        *recv_len = lm;
+        OPENSSL_free(decr);
+    }
+    if(isVerbose) fprintf(stdout,"rxData() :Ricevuti da %s bytes = %lu [%.20s...]\n", ip_address, *recv_len, buffer);
+    return(TRUE);
+}   
+
+// ---- INvio della risposta con eventuale criptatura
+//
+int txData(int sockfd, char *buffer, size_t *txlen, 
+                char *ip_address, struct sockaddr_in server_addr, 
+                EVP_PKEY *keypair) {
+
+    ssize_t sentLen = 0;
+    if(keypair != NULL) { // bisogna crittografare 
+        unsigned char *decr;
+        size_t lm;
+        if(doEncrypt(keypair, (const unsigned char *)buffer, *txlen, &decr, &lm) == FALSE) {
+            fprintf(stderr,"txData() : Errore di cifratura!");
+            return(FALSE);
+        }
+        if(decr != NULL) memcpy(buffer, decr, lm);
+        buffer[lm] = '\0';
+        *txlen = lm;
+        OPENSSL_free(decr);
+    }
+    sentLen = sendto(sockfd, buffer, *txlen, 0, (const struct sockaddr *)(&server_addr), sizeof(server_addr));
+    if(sentLen <= 0) {
+        fprintf(stderr,"txData() : Errore di trasmissione %zd bytes in spedizione, 0 inviati!", sentLen);
+    }
+    if(isVerbose) fprintf(stdout,"txData() : Trasmesso a %s byte = %lu [%.20s...]\n", ip_address, *txlen, buffer);
+    *txlen = (size_t)sentLen;
+    return(TRUE);
+}
+
+// Bussa al server  
+//
+//
+int clientKnock(int sockfd, char *pubPEMKey, size_t *keylen, char *theServerIp, EVP_PKEY *keypair,
+                OSSL_LIB_CTX *libctx, struct sockaddr_in server_addr, char *theMessage) {
+    char buf[10];
+
+    if(txData(sockfd, pubPEMKey, keylen, theServerIp, server_addr, NULL) == FALSE) { // chiave pubblica
+        fprintf(stderr,"Errore di trasmissione !\n");
+        return(FALSE);
+    }
+    if(isVerbose) fprintf(stdout,"Chiave pubblica inviata a:%s \n", theServerIp);
+    if(rxData(sockfd, pubPEMKey, keylen, theServerIp, NULL) == FALSE) { // server pub key ricevuta
+        return(FALSE);
+    }
+    EVP_PKEY *keyServPub = setupPublicKey(libctx, NULL, TRUE, "/tmp/pubserverkey.pem",
+                                         (const unsigned char *)pubPEMKey, *keylen); // memorizza la chiave pubblica
+    if(keyServPub == NULL) { 
+        fprintf(stderr,"Chiave pubblica non valida !\n");
+        return(FALSE);
+    }
+    if(isVerbose) fprintf(stdout,"Chiave  pubblica ricevuta da:%s \n", theServerIp);
+        
+    size_t txlen = strlen((const char *)theMessage);
+    if(txData(sockfd, (char *)theMessage, &txlen, theServerIp, server_addr, keyServPub) == FALSE) {
+        fprintf(stderr,"Errore di trasmissione !\n");
+        return(FALSE);
+    }
+    if(rxData(sockfd, pubPEMKey, keylen, theServerIp, keypair) == FALSE) {  // leggi la risposta 
+        fprintf(stderr,"Errore di ricezione !\n");
+        return(FALSE);
+    }
+    //printf(">>> %lu %lu %lu \n\n", keypair,pubPEMKey); 
+    txlen = 3;
+    strcpy(buf,"Bye");
+    if(txData(sockfd, buf, &txlen, theServerIp, server_addr, NULL) == FALSE) {
+        fprintf(stderr,"Errore di trasmissione !\n");
+        return(FALSE);
+    }
+    return(TRUE);
+}
+
+#endif
+// --------  EOF ---------
---
+++ src/lnid-ssl.c
@@ -0,0 +1,328 @@
+/* ----------------------------------------------------------
+    LNID - Local Network Identity Discovery
+
+   Libreria funzioni supporto SSL
+
+ Copyright (c) 2024 Antonio Franco
+
+ Questo programma è rilasciato sotto la licenza Creative Commons Attribuzione 4.0 Internazionale (CC BY 4.0).
+ Puoi condividere, copiare, distribuire e modificare il programma, a condizione di dare il dovuto credito all'autore originale.
+
+ Licenza completa: https://creativecommons.org/licenses/by/4.0/
+ 
+ auth. A.Franco - INFN Bary Italy
+ date: 28/11/2024       ver.1.1
+
+ ---------------------------------------------------------
+  HISTORY 
+  28/11/2024  -  Creation
+
+ ---------------------------------------------------------
+*/
+#ifndef __SSLSUPPORT__
+#define __SSLSUPPORT__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+#include <openssl/err.h>
+#include <openssl/encoder.h>
+#include <openssl/decoder.h>
+#include <openssl/evp.h>
+#include <openssl/core_names.h>
+#include <openssl/provider.h>
+
+#define PRIVKEYFILES "/tmp/privateserkey.pem"
+#define PUBKEYFILES "/tmp/publicserkey.pem"
+#define PRIVKEYFILEC "/tmp/privateclikey.pem"
+#define PUBKEYFILEC "/tmp/publicclikey.pem"
+
+#define KEY_SIZE 2048
+#define EXPONENT RSA_F4
+#define PADDING RSA_PKCS1_OAEP_PADDING
+// -----------
+
+#define FALSE 0
+#define TRUE 1
+#define BUFFER_SIZE 4096
+
+// Variabili globali
+extern int isVerbose; // definito nel main program 
+static const char *propq = NULL;
+
+// stampa l'errore
+void errorAndExit(const char* msg) {
+    fprintf(stderr, "Errore : %s\n", msg);
+    char buf[256];
+    int err = ERR_get_error();
+    ERR_error_string_n(err, buf, sizeof(buf));
+    fprintf(stderr, "err no: %d , %s\n", err, buf);
+    exit(EXIT_FAILURE);
+}
+
+// ---- Genera la coppia di chiavi Priv e Pub
+// Ret: NULL per errore
+EVP_PKEY *generateRsaKeyPair(unsigned int bits) {
+    const uint32_t exponent = 0x10001;
+    EVP_PKEY_CTX* ctx = EVP_PKEY_CTX_new_id(EVP_PKEY_RSA, NULL);
+    if(ctx == NULL) { errorAndExit("generateRsaKeyPair() : Could not create a context for RSA_PSS"); }
+    if(EVP_PKEY_keygen_init(ctx) <= 0) { errorAndExit("generateRsaKeyPair() : Could not initialize the RSA context"); }
+    if(EVP_PKEY_CTX_set_rsa_keygen_bits(ctx, bits) <= 0) { errorAndExit("generateRsaKeyPair() : EVP_PKEY_CTX_set_rsa_keygen_bits failed"); }
+    BIGNUM* exponent_bn = BN_new();
+    BN_set_word(exponent_bn, exponent);
+    if(EVP_PKEY_CTX_set_rsa_keygen_pubexp(ctx, exponent_bn) <= 0) { errorAndExit("generateRsaKeyPair() : EVP_PKEY_CTX_set_rsa_keygen_pubexp failed"); }
+    EVP_PKEY* pkey = NULL;
+    if(EVP_PKEY_keygen(ctx, &pkey) != 1) { errorAndExit("generateRsaKeyPair() : EVP_PKEY_keygen failed"); }
+    EVP_PKEY_CTX_free(ctx);
+    return(pkey);
+}
+
+void dumpKeyPair(EVP_PKEY *keypair) {
+    printf("----- Key Pair Dump ------\n");
+    EVP_PKEY_print_public_fp(stdout, keypair, 1, NULL);
+    EVP_PKEY_print_private_fp(stdout, keypair, 1, NULL); 
+    printf("---------   EOF  ----------\n");
+    return;
+}
+
+// --- crea due file PEM a partire da una struttura EVP_PKEY
+//  Ret = FALSE per errore
+int storeRSAKeyPair(EVP_PKEY *keypair, const char *publicKeyPEM, const char *privateKeyPEM) {
+    FILE *fp = NULL;  
+    fp = fopen(publicKeyPEM, "w"); // Output a PEM encoding of the public key. 
+    if(fp == NULL) { 
+        fprintf(stderr, "storeRsaKeyPair() : Errore creazione file PEM chiave pubblica = %s",publicKeyPEM);
+        return FALSE;
+    }
+    if (PEM_write_PUBKEY(fp, keypair) == 0) {
+        fprintf(stderr, "storeRsaKeyPair() : Errore di scrittura PEM-encoded della chiave pubblica\n");
+        fclose(fp);
+        return FALSE;
+    }
+    fclose(fp);
+
+    fp = fopen(privateKeyPEM, "w"); // Output a PEM encoding of the private key. 
+    if(fp == NULL) { 
+        fprintf(stderr, "storeRsaKeyPair() : Errore creazione file PEM chiave privata = %s",privateKeyPEM); 
+        return FALSE;
+    }
+    if (PEM_write_PrivateKey(fp, keypair, NULL, NULL, 0, NULL, NULL) == 0) {
+        fprintf(stderr, "storeRsaKeyPair() : Errore di scrittura PEM-encoded della chiave privata\n");
+        fclose(fp);
+        return FALSE;
+    }
+    fclose(fp);
+    return(TRUE);
+}
+
+// ---- Carica la chiave da un file PEM
+// Ret = NULL per errore
+EVP_PKEY *loadKeyFromPEM(OSSL_LIB_CTX *libctx, const char *fileName, const char *passphrase)
+{
+    int ret = 0;
+    EVP_PKEY *pkey = NULL;
+    OSSL_DECODER_CTX *dctx = NULL;
+    int selection = 0; // Auto selection 
+
+    FILE *fp = NULL;
+    fp = fopen(fileName, "rb");
+    if(fp == NULL) { 
+        fprintf(stderr, "loadKeyFromPEM() : Errore lettura file PEM : %s\n", fileName);
+        goto cleanup;
+    }
+    dctx = OSSL_DECODER_CTX_new_for_pkey(&pkey, "PEM", NULL, "RSA", selection, libctx, propq);
+    if (dctx == NULL) {
+        fprintf(stderr, "loadKeyFromPEM() : errore OSSL_DECODER_CTX_new_for_pkey() !\n");
+        goto cleanup;
+    }
+    if (passphrase != NULL) {
+        if (OSSL_DECODER_CTX_set_passphrase(dctx, (const unsigned char *)passphrase, strlen(passphrase)) == 0) {
+            fprintf(stderr, "loadKeyFromPEM() : errore OSSL_DECODER_CTX_set_passphrase() !\n");
+            goto cleanup;
+        }
+    }
+    if (OSSL_DECODER_from_fp(dctx, fp) == 0) { 
+        fprintf(stderr, "loadKeyFromPEM() : errore OSSL_DECODER_from_fp() !\n");
+        goto cleanup;
+    }
+    ret = 1;
+cleanup:
+    if(dctx != NULL) OSSL_DECODER_CTX_free(dctx);
+    if(fp != NULL) fclose(fp);
+    if (ret == 0) {
+        EVP_PKEY_free(pkey);
+        pkey = NULL;
+    }
+    return pkey;
+}
+
+// ---- Regista la chiave in un file PEM
+// La selection controlla sela private key e' esportata
+// EVP_PKEY_KEYPAIR, o solo la pubblica EVP_PKEY_PUBLIC_KEY
+// Ret = NULL per errore
+int storeKeyInPEM(EVP_PKEY *pkey, const char *fileName, int selection, const char *passphrase)
+{
+    int ret = 0;
+    OSSL_ENCODER_CTX *ectx = NULL;
+
+    FILE *fp = NULL;
+    fp = fopen(fileName, "w");
+    if(fp == NULL) { 
+        fprintf(stderr, "storeKeyInPEM() : Errore creazione del file PEM : %s\n", fileName);
+        goto cleanup;
+    }
+    // Crea un PEM encoder ctx.
+    // selection = (passphrase != NULL) ? EVP_PKEY_KEYPAIR : EVP_PKEY_PUBLIC_KEY;
+    ectx = OSSL_ENCODER_CTX_new_for_pkey(pkey, selection, "PEM", NULL, propq);
+    if (ectx == NULL) {
+        fprintf(stderr, "storeKeyInPEM() : OSSL_ENCODER_CTX_new_for_pkey() errore!\n");
+        goto cleanup;
+    }
+    // setta la passward 
+    if (passphrase != NULL) {
+        if (OSSL_ENCODER_CTX_set_cipher(ectx, "AES-128-CBC", propq) == 0) {
+            fprintf(stderr, "storeKeyInPEM() : OSSL_ENCODER_CTX_set_cipher() failed\n");
+            goto cleanup;
+        }
+        if (OSSL_ENCODER_CTX_set_passphrase(ectx, (const unsigned char *)passphrase, strlen(passphrase)) == 0) {
+            fprintf(stderr, "storeKeyInPEM() : OSSL_ENCODER_CTX_set_passphrase() failed\n");
+            goto cleanup;
+        }
+    }
+    if (OSSL_ENCODER_to_fp(ectx, fp) == 0) { // Codifica e scrittura nel file 
+        fprintf(stderr, "storeKeyInPEM() : OSSL_ENCODER_to_fp() failed\n");
+        goto cleanup;
+    }
+    ret = 1;
+cleanup:
+    fclose(fp);
+    OSSL_ENCODER_CTX_free(ectx);
+    return ret;
+}
+
+// Esegue la decriptatura con chiave privata
+//
+//
+static int doDecrypt(EVP_PKEY *privateKey,
+                      const unsigned char *in, size_t in_len,
+                      unsigned char **out, size_t *out_len)
+{
+    EVP_PKEY_CTX* dec_ctx = EVP_PKEY_CTX_new(privateKey, NULL);
+    if(EVP_PKEY_decrypt_init(dec_ctx) <= 0) {
+        errorAndExit("doDecrypt() : EVP_PKEY_encrypt_init errore !");
+    }
+    if(EVP_PKEY_CTX_set_rsa_padding(dec_ctx, RSA_PKCS1_OAEP_PADDING) <= 0) {
+        errorAndExit("doDecrypt() : EVP_PKEY_CTX_set_rsa_padding errore !");
+    }
+    // calcola la dimensione
+    if(EVP_PKEY_decrypt(dec_ctx, NULL, out_len, in, in_len) <= 0) {
+        errorAndExit("doDecrypt() : EVP_PKEY_decrypt dry errore !");
+    }
+    // alloca il buffer
+    size_t buf_len = *out_len;
+    unsigned char *buf = NULL;
+    buf = OPENSSL_malloc(buf_len);
+    if(buf == NULL) {
+        errorAndExit("doDecrypt() : Errore di allocazione OPENSSL_malloc().\n");
+    }
+    // Decifra
+    if(EVP_PKEY_decrypt(dec_ctx, buf, &buf_len, in, in_len) <= 0) {
+        erroAndExit("doDecrypt() : EVP_PKEY_decrypt failed");
+    }
+    *out_len = buf_len;
+    *out = buf;
+    if(isVerbose) {
+        fprintf(stdout, "Decifrati %d bytes:\n",buf_len);
+        BIO_dump_indent_fp(stdout, buf, buf_len, 2);
+        fprintf(stdout, "\n");
+    }
+    EVP_PKEY_CTX_free(dec_ctx);
+    return(TRUE);
+}
+
+// Esegue la criptatura con chiave pubblica
+//
+//
+static int doEncrypt(EVP_PKEY *publicKey,
+                     const unsigned char *in, size_t in_len,
+                     unsigned char **out, size_t *out_len)
+{
+    // Crea un nuovo contesto per criptare.
+    EVP_PKEY_CTX* enc_ctx = EVP_PKEY_CTX_new(publicKey, NULL);
+    if (EVP_PKEY_encrypt_init(enc_ctx) <= 0) {
+        errorAndExit("doEncrypt() : EVP_PKEY_encrypt_init errore !");
+    }
+    if (EVP_PKEY_CTX_set_rsa_padding(enc_ctx, RSA_PKCS1_OAEP_PADDING) <= 0) {
+        errorAndExit("doEncrypt() : EVP_PKEY_CTX_set_rsa_padding errore !");
+    }
+    // Determina la dimensione dell'output
+    if (EVP_PKEY_encrypt(enc_ctx, NULL, out_len, in, in_len) <= 0) {
+        errorAndExit("doEncrypt() : dry EVP_PKEY_encrypt errore !");
+    }
+    // alloca il buffer
+    size_t buf_len = *out_len;
+    unsigned char *buf = NULL;
+    buf = OPENSSL_malloc(buf_len);
+    if(buf == NULL) {
+        errorAndExit("doEncrypt() : Errore di allocazione OPENSSL_malloc().\n");
+    }
+    // Cifratura
+    if(EVP_PKEY_encrypt(enc_ctx, buf, &buf_len, in, in_len) <= 0) {
+        errorAndExit("doEncrypt() : EVP_PKEY_encrypt errore !");
+    }
+    *out_len = buf_len;
+    *out = buf;
+    if(isVerbose) {
+        fprintf(stdout, "Cifrati %d bytes :\n",buf_len);
+        BIO_dump_indent_fp(stdout, buf, buf_len, 2);
+        fprintf(stdout, "\n");
+    }
+    EVP_PKEY_CTX_free(enc_ctx);
+    return(TRUE);
+}
+
+
+
+// ---- Libera la memoria allocata per le chiavi RSA
+//
+void freeRsaKeyPair(EVP_PKEY *keypair) {
+    EVP_PKEY_free(keypair);   
+    return;
+}
+
+EVP_PKEY *setupPublicKey(OSSL_LIB_CTX *libctx, const char *propq,
+                            int public, const char *fileName,
+                            const unsigned char *data, size_t data_len)
+{
+    OSSL_DECODER_CTX *dctx = NULL;
+    EVP_PKEY *pkey = NULL;
+    int selection;
+
+    if (public == TRUE) {
+        selection = EVP_PKEY_PUBLIC_KEY;
+    } else {
+        selection = EVP_PKEY_KEYPAIR;
+    }
+    dctx = OSSL_DECODER_CTX_new_for_pkey(&pkey, "DER", NULL, "RSA",
+                                         selection, libctx, propq);
+    (void)OSSL_DECODER_from_data(dctx, &data, &data_len);
+    OSSL_DECODER_CTX_free(dctx);
+
+    FILE *fp = fopen(fileName, "w");
+    if(fp == NULL) { 
+        fprintf(stderr, "setupPublicKey() : errore di creazione del file %s !\n",fileName);
+        return(pkey); 
+    }
+    size_t wr = fwrite(data, data_len, 1, fp);
+    if(data_len != wr) {
+        fprintf(stderr, "setupPublicKey() : errore di scrittura nel file %s !\n",fileName);
+    }
+    fclose(fp);
+    return(pkey);
+}
+
+#endif
+// --------  EOF ---------
